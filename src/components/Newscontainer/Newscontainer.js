1440905bd3329c5d57a4869fbda869d1ddcec898
Harshit Vidhyarthi <88877088+Harshit8319127699@users.noreply.github.com>
2022-05-17 23:10:03 +0530

    Update Newscontainer.js

--- src/components/Newscontainer/Newscontainer.js
+++ src/components/Newscontainer/Newscontainer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Economic from '../economics/Economic'
 import Entertainment from '../entertainment/Entertainment'
@@ -9,6 +9,25 @@ import Politics from '../politics/Politics'
 import Science from '../science/Science'
 import Sports from '../sports/Sports'
 function Newscontainer() {
+    const [first, setfirst] = useState([])
+    
+    
+    useEffect(() => {
+        fetch( "https://newsapi.org/v2/everything?q=world&apiKey=fea5587a2bc24f31b14228e9c7b8c4a9"
+        ).then(res=>res.json())
+        .then(response=>{
+            setfirst(response.articles)
+        })
+      
+    }, [])
+
+    const holder=first.map((elem,id)=>{
+        if(elem.content!==null){
+            return <div className="topnews" key={id}>
+            <h5>Headline: {elem.content}</h5>
+        </div>
+        }    
+    })
     
     return (
         <div className="container">
@@ -25,6 +44,10 @@ function Newscontainer() {
 
 
             <div className="right">
+<div className="heading">
+    <h3>Top Headlines</h3>
+</div>
+{holder}
                             </div>
         </div>
     )
